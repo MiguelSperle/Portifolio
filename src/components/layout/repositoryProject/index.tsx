4cc5786b5dc8b65ab2ee11214d237c50e5b49541
MiguelSperle <miguelsperle205@yahoo.com>
2023-03-11 19:02:38 -0300

    Removed only one project

--- src/components/layout/repositoryProject/index.tsx
+++ src/components/layout/repositoryProject/index.tsx
@@ -10,11 +10,6 @@ export default function Project(){
                     </div>
 
                     <div className="project">
-                        <a href="https://github.com/MiguelSperle/Card" target='_blank'><p>Github Card</p></a>
-                        <p>JAN 23, 2023</p>
-                    </div>
-
-                    <div className="project">
                         <a href="#" target='_blank'><p>Buscar CEP</p></a>
                         <p>MAR 11, 2023</p>
                     </div>
