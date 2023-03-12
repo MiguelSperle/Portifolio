2aac33f457acffd9effbb974c0bb802d01dc694e
MiguelSperle <miguelsperle205@yahoo.com>
2023-03-12 17:53:48 -0300

    Add link of project

--- src/components/layout/repositoryProject/index.tsx
+++ src/components/layout/repositoryProject/index.tsx
@@ -5,12 +5,12 @@ export default function Project(){
                 <h2>Meus Principais Repósitórios</h2>
                 <div className="container-all-project">
                     <div className="project">
-                        <a href="https://github.com/MiguelSperle/HXZstore" target='_blank'><p>E-commerce</p></a>
+                        <a href="https://github.com/MiguelSperle/HXZstore" target='_blank'><a className="linkProject" href="https://hxz-store-fawn.vercel.app/" target='_blank'>#</a><p>E-commerce</p></a>
                         <p>FEB 7, 2023</p>
                     </div>
 
                     <div className="project">
-                        <a href="https://github.com/MiguelSperle/Search-CEP" target='_blank'><p>Buscar CEP</p></a>
+                        <a href="https://github.com/MiguelSperle/Search-CEP" target='_blank'><a className="linkProject" href="https://search-cep-indol.vercel.app/" target='_blank'>#</a><p>Buscar CEP</p></a>
                         <p>MAR 11, 2023</p>
                     </div>
                 </div>
